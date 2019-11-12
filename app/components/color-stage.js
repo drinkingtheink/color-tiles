cfd06c2067972f7858efc238c70b8e2007a468d2
Jason M Harrison <jason.m.harrison@gmail.com>
2019-11-11 18:45:48 -0600

    Displaying colors based on count with a WIP button to re-colorize.

--- app/components/color-stage.js
+++ app/components/color-stage.js
@@ -2,16 +2,29 @@ import Component from '@ember/component';
 import { colors } from '../data/colors';
 
 export default Component.extend({
+  count: 500,
+  structure: null,
   currentColor: null,
   init() {
     this._super(...arguments);
-    alert(`GETTING COLORS`);
-    this.getRandomColors();
+    this.createStruture();
+  },
+  getRandomColor () {
+    let colorSet = colors[Math.floor(Math.random()*colors.length)];
+    return colorSet;
+  },
+  createStruture () {
+    let structure = [];
+    let count = this.get('count');
+    let i;
+    for (i = 0; i < count; i++) { 
+      structure.push(this.getRandomColor());
+    }
+    this.set('structure', structure);
   },
   actions: {
-    getRandomColors () {
-      let colorSet = myArray[Math.floor(Math.random()*myArray.length)];
-      this.set('currentColor', colorSet);
+    resetStructure () {
+      this.createStruture();
     }
   }
 });
